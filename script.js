b94c61f9347e93afc27f82ff678c2a50e13c66d0
SharvariPadiyar26 <sharvaripadiyar@gmail.com>
2025-09-05 02:17:34 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Change navbar style on scroll
-window.addEventListener("scroll", function() {
-  const navbar = document.getElementById("navbar");
-  navbar.classList.toggle("scrolled", window.scrollY > 50);
-});
